f8229bfc2a3cc21ae4815138d8f9e094116fa26e
alien fisherman <57496995+2na3k@users.noreply.github.com>
2026-09-12 21:32:50 +0800

    Add: number-of-senior-citizens - submission-4

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-4.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        def func(detail: str) -> bool:
+            age_str = detail[11:13]
+            return int(age_str) > 60
+
+        res = map(func, details)
+        return sum(list(res))
